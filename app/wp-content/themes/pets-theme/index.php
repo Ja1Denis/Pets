e4b9492b01dd8c31a420e40bb90b0b4fe4d92915
denis <sdenis.vr@gmail.com>
2018-02-22 17:48:49 +0100

    Learning arrays in php

--- app/wp-content/themes/pets-theme/index.php
+++ app/wp-content/themes/pets-theme/index.php
@@ -1,5 +1,13 @@
 <?php
 $names= array('Denis' , "Martina","Izabela","Sintia");
 
+$count = 1;
+
+while ($count < 101) {
+  # code...
+  echo "<li>$count</li>";
+  $count++;
+}
+
+
  ?>
-<p>Hi,my name is <?php echo $names[1]  ?>.</p>
