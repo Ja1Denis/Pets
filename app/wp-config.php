6e9ff0938e63b7b8adb19ab0aaa09b1958e88fd9
denis <sdenis.vr@gmail.com>
2018-02-23 19:37:27 +0100

    Converting static HTML temlate into WordPress.

--- app/wp-config.php
+++ app/wp-config.php
@@ -79,6 +79,12 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+/*Increase PHP Memory Limit in WordPress*/
+define( 'WP_MEMORY_LIMIT', '500M' );
+
+/*Maximum execution time*/
+set_time_limit(600);
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
--- app/wp-content/themes/pets-theme/functions.php
+++ app/wp-content/themes/pets-theme/functions.php
@@ -1,6 +1,10 @@
 <?php
 /*dodajem funkciju koja učitava sve css,javascript itd scripte*/
 function pets_files() {
-  wp_enqueue_style("pets_main_styles", get_stylesheet_uri(),NULL,microtime());/*delete NULL and microtime() when you push this site live ,check this Udemy link https://goo.gl/RLkydt*/
+  wp_enqueue_scripts("main-pets-js",get_theme_file_uri("/js/scripts-bundled.js"),NULL,"1.0",true);
+  wp_enqueue_style("custom-google-fonts","//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
+  wp_enqueue_style("font-awesome","//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+  wp_enqueue_style("pets_main_styles",get_stylesheet_uri(),NULL,microtime());
+  /*delete NULL and microtime() when you push this site live ,check this Udemy link https://goo.gl/RLkydt*/
 }
 add_action("wp_enqueue_scripts","pets_files");
