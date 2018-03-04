bb62c223d52550be1e216598f98299978aa87910
denis <sdenis.vr@gmail.com>
2018-03-04 12:05:39 +0100

    Manipulating Default URL Based Queries

--- app/wp-content/themes/pets-theme/functions.php
+++ app/wp-content/themes/pets-theme/functions.php
@@ -17,3 +17,24 @@ function pets_features() {
 }
 
 add_action("after_setup_theme","pets_features");
+
+
+function pets_adjust_queries($query) {
+  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key','event_date');
+    $query->set('orderby','meta_value_num');
+    $query->set('order','ASC');
+    $query->set('meta_query', array(
+      array(
+        'key'=> 'event_date',
+        'compare'=> '>=',
+        'value' => $today,
+        'type'=> 'numeric'
+      )
+    ));
+
+
+  }
+}
+add_action('pre_get_posts','pets_adjust_queries');
