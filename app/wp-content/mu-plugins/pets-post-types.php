ca7f16ff68fc1e0e8ddab8648d90173c3385992d
denis <sdenis.vr@gmail.com>
2018-03-05 19:54:03 +0100

    Creating 'Program' Post Type ,and creating Relationships between content

--- app/wp-content/mu-plugins/pets-post-types.php
+++ app/wp-content/mu-plugins/pets-post-types.php
@@ -3,6 +3,7 @@
 
 function pets_post_types()  {
   register_post_type('event', array(
+  //Event Post Type
 'supports' => array('title','editor','excerpt'),    
 'rewrite'=> array('slug' => 'events'),
     'has_archive' => true,
@@ -16,6 +17,24 @@ function pets_post_types()  {
     ),
     'menu_icon' => 'dashicons-calendar'
   ));
+
+  //Program Post Type
+   
+   register_post_type('program', array(
+'supports' => array('title','editor'),    
+'rewrite'=> array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+       'name' => 'Programs',
+       'add_new_item'=> 'Add New Program',
+       'edit_item'=>'Edit Program',
+       'all_items'=>'All Programs',
+       'singular_name'=>'Program'
+    ),
+    'menu_icon' => 'dashicons-awards'
+  ));
+
 }
 
 add_action('init','pets_post_types');
--- app/wp-content/themes/pets-theme/functions.php
+++ app/wp-content/themes/pets-theme/functions.php
@@ -20,6 +20,13 @@ add_action("after_setup_theme","pets_features");
 
 
 function pets_adjust_queries($query) {
+  if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+
+   $query->set('orderby','title');
+   $query->set('order','ASC');
+   $query->seeet('post_per_page',-1);
+  }
+
   if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
     $today = date('Ymd');
     $query->set('meta_key','event_date');
