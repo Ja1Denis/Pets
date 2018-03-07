aeed9f3985daa510f814c7bfce9a3b74998c2d23
denis <sdenis.vr@gmail.com>
2018-03-07 22:20:02 +0100

    Featured Image (Post Thumbnail) Part 2 Fixes on thumnails

--- app/wp-content/themes/pets-theme/single-program.php
+++ app/wp-content/themes/pets-theme/single-program.php
@@ -51,9 +51,9 @@ while(have_posts()) {
         while($relatedProfessors->have_posts()) {
           $relatedProfessors->the_post(); ?>
 
-          <li class="professor_card__list-item">
+          <li class="professor-card__list-item">
             <a class="professor-card" href="<?php the_permalink();?>">
-              <img class="professo-card__image" src="<?php the_post_thumbnail_url() ?>">
+              <img class="professor-card__image" src="<?php the_post_thumbnail_url() ?>">
               <span class="professor-card__name"><?php the_title();  ?></span>
             </a>
           </li>
