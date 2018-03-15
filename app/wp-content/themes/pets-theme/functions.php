c8f073a1f0a78fc65dece4001ba69744b1de4d81
denis <sdenis.vr@gmail.com>
2018-03-15 14:08:27 +0100

    Campus Map on the front end

--- app/wp-content/themes/pets-theme/functions.php
+++ app/wp-content/themes/pets-theme/functions.php
@@ -38,6 +38,7 @@ function pageBanner($args = NULL){
 function pets_files() {
   wp_enqueue_style("font-awesome","//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
   wp_enqueue_style("custom-google-fonts","//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/map/api/js?key=', null, microtime(), true );
   wp_enqueue_script('main-pets-js', get_theme_file_uri( '/js/scripts-bundled.js' ), null, microtime(), true );
   wp_enqueue_style("pets_main_styles",get_stylesheet_uri(),NULL,microtime());
 
