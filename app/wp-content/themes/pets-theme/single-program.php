608eeb2ca3f3af7ad68793d5aa7537f708f9d06d
denis <sdenis.vr@gmail.com>
2018-03-15 22:47:21 +0100

    When we are on 'Program' page we now have related campuses

--- app/wp-content/themes/pets-theme/single-program.php
+++ app/wp-content/themes/pets-theme/single-program.php
@@ -88,7 +88,27 @@ while(have_posts()) {
           while($hompageEvents->have_posts()) {
             $hompageEvents->the_post();
             get_template_part('template-parts/content-event');
-     }
+        }
+        }
+        wp_reset_postdata();
+        $relatedCampuses = get_field('related_campus');
+
+        if ($relatedCampuses){
+          echo '<hr class="section-break" />';
+          echo  '<h2 class="headline headline--medium">' . get_the_title() . ' Is Avalible At This Campuses: </h2>';
+
+        echo '<ul class="min-list link-list">';
+          foreach($relatedCampuses as $campus) {
+            ?>
+            <li>
+                <a href="<?php echo get_the_permalink($campus); ?>">
+                   <?php echo get_the_title($campus) ?>
+                </a>
+            </li>
+            <?php
+
+          }
+          echo '</ul>';
         }
        ?>
   </div>
